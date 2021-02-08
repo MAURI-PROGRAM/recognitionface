c5da7f5d83b34c448d3b32b33c116662fc6f28f0
MICHAEL MERCHAN <32249510+MAURI-PROGRAM@users.noreply.github.com>
2021-02-08 18:33:39 -0500

    Update CognitiveVisionService.cs

--- Services/CognitiveVisionService.cs
+++ Services/CognitiveVisionService.cs
@@ -6,6 +6,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using System;
+using System.IO;
 using Api_face_recognition.Domain;
 using Microsoft.Azure.CognitiveServices.Vision.Face;
 using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
@@ -22,19 +23,54 @@ namespace Api_face_recognition.Services
 
         public  async Task<List<DetectedFace>> DetectFaceRecognize( string url, string recognition_model)
         {
-            var requiredFaceAttributes = new FaceAttributeType?[] {
-                FaceAttributeType.Age,
-                FaceAttributeType.Gender
-            };
-            IList<DetectedFace> detectedFaces = await _client.Face.DetectWithUrlAsync(url, recognitionModel: recognition_model,returnFaceAttributes: requiredFaceAttributes, detectionModel: DetectionModel.Detection01);
+            IList<FaceAttributeType?> faceAttributes =
+                new FaceAttributeType?[]
+                {
+                    FaceAttributeType.Age, FaceAttributeType.Gender,
+
+                };
+            IList<DetectedFace> detectedFaces = await _client.Face.DetectWithUrlAsync(url, recognitionModel: recognition_model,returnFaceAttributes: faceAttributes, detectionModel: DetectionModel.Detection01);
             return detectedFaces.ToList();
-        }  
- 
+        } 
+
+        public  async Task<List<DetectedFace>> DetectFaceRecognizeStream( Stream stream, string recognition_model)
+        {
+            IList<DetectedFace> detectedFaces = await _client.Face.DetectWithStreamAsync(stream, true, true, null);
+            return detectedFaces.ToList();
+        } 
 
         public  async Task<VerifyResult> VerifyTwoFaces( Guid FaceId1, Guid FaceId2)
         {
             VerifyResult verifyObjectResult = await _client.Face.VerifyFaceToFaceAsync(FaceId1, FaceId2);
             return verifyObjectResult;
-        } 
+        }
+
+        public  Boolean EyesBlink( List<DetectedFace> facesDetected )
+        {
+            Console.WriteLine(facesDetected.Count);
+            var facelandmarks = facesDetected[0].FaceLandmarks;
+            Console.WriteLine("face"+facelandmarks.EyeLeftInner.X);
+                    Double  difLeftX = Math.Sqrt(
+                                    Math.Pow(facelandmarks.EyeLeftInner.X-facelandmarks.EyeLeftOuter.X,2)+
+                                    Math.Pow(facelandmarks.EyeLeftInner.Y-facelandmarks.EyeLeftOuter.Y,2));
+                    Double  difLeftY = Math.Sqrt(
+                                    Math.Pow(facelandmarks.EyeLeftBottom.X-facelandmarks.EyeLeftTop.X,2)+
+                                    Math.Pow(facelandmarks.EyeLeftBottom.Y-facelandmarks.EyeLeftTop.Y,2));
+
+                    Double difRightX = Math.Sqrt(
+                                    Math.Pow(facelandmarks.EyeRightInner.X-facelandmarks.EyeRightOuter.X,2)+
+                                    Math.Pow(facelandmarks.EyeRightInner.Y-facelandmarks.EyeRightOuter.Y,2));
+                    Double difRightY = Math.Sqrt(
+                                    Math.Pow(facelandmarks.EyeRightBottom.X-facelandmarks.EyeRightTop.X,2)+
+                                    Math.Pow(facelandmarks.EyeRightBottom.Y-facelandmarks.EyeRightTop.Y,2));
+                    
+                    Double leftEAR = difLeftY / difLeftX;
+                    Double rightEAR = difRightY/difRightX;
+                    Double EYE_AR_THRESH = 0.3;
+                    bool leftBlink = leftEAR < EYE_AR_THRESH;
+                    bool rightBlink = rightEAR < EYE_AR_THRESH;
+            return leftBlink || rightBlink;
+        }
+        
     }
 }
