32c716c3628164ccda9a757114b0acddd8a1982d
MICHAEL MERCHAN <32249510+MAURI-PROGRAM@users.noreply.github.com>
2021-02-08 18:33:13 -0500

    Update ICognitiveVisionService.cs

--- Services/ICognitiveVisionService.cs
+++ Services/ICognitiveVisionService.cs
@@ -5,13 +5,15 @@ using Api_face_recognition.Domain;
 using System.Threading.Tasks;
 using Microsoft.Azure.CognitiveServices.Vision.Face;
 using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System.IO;
 
 namespace Api_face_recognition.Services
 {
     public interface ICognitiveVisionService
     {
         Task<List<DetectedFace>> DetectFaceRecognize( string url, string recognition_model);
-
+        Task<List<DetectedFace>> DetectFaceRecognizeStream( Stream stream, string recognition_model);
         Task<VerifyResult> VerifyTwoFaces( Guid FaceId1, Guid FaceId2);
+        Boolean EyesBlink( List<DetectedFace> facesDetected );
     }
 }
